59c1d50f12dd4a4809b4827261703000ab08a400
Nahush Shrivatsa <67535204+nahushvatsa@users.noreply.github.com>
2021-03-29 03:19:29 +0400

    Update totalequipment.sql

--- functionwise/sql/totalequipment.sql
+++ functionwise/sql/totalequipment.sql
@@ -7,6 +7,12 @@ CREATE TABLE totalequipment (
   Price int DEFAULT NULL
 ) 
 
-LOCK TABLES `totalequipment` WRITE;
+-- Dumping data for table totalequipment
 
-INSERT INTO `totalequipment` VALUES ('Beaker',25,25,10),('Test Tube',30,30,5),('Watchglass',15,15,5),('Flask',20,20,10),('Funnel',15,15,7),('Burette',10,10,15),('Pipette',10,10,15);
+INSERT INTO totalequipment VALUES ('Beaker',25,25,10);
+INSERT INTO totalequipment VALUES ('Test Tube',30,30,5);
+INSERT INTO totalequipment VALUES ('Funnel',15,15,7);
+INSERT INTO totalequipment VALUES ('Burette',10,10,15);
+INSERT INTO totalequipment VALUES ('Pipette',10,10,15);
+INSERT INTO totalequipment VALUES ('Watchglass',15,15,5)
+INSERT INTO totalequipment VALUES ('Flask',20,20,10);
