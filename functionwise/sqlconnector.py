bbd5ee055e51dcb8fb47007f304899616c9b5280
Nahush Shrivatsa <67535204+nahushvatsa@users.noreply.github.com>
2021-03-28 23:18:54 +0400

    Update sqlconnector.py

--- functionwise/sqlconnector.py
+++ functionwise/sqlconnector.py
@@ -1,8 +1,8 @@
 import mysql.connector
-mydb = mysql.connector.connect(
-  host="localhost3306",
+mydb = mysql.connector.connect(      #String values given here are assuming the user is connecting to a local SQL server. 
+  host="localhost",                  #Ergo, values may change user by user depending on their SQL server.
   user="root",
-  password="tiger",
+  password="[your password]",        #String value for password is acting as a placeholder here. Use your MySQL's password. 
   database="chemlab"
 )
 
