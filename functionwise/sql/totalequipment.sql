ba1a38096b2e77759659c17264f7c3e789c3bdfa
Nahush Shrivatsa <67535204+nahushvatsa@users.noreply.github.com>
2021-03-29 03:23:02 +0400

    Update totalequipment.sql

--- functionwise/sql/totalequipment.sql
+++ functionwise/sql/totalequipment.sql
@@ -14,5 +14,5 @@ INSERT INTO totalequipment VALUES ('Test Tube',30,30,5);
 INSERT INTO totalequipment VALUES ('Funnel',15,15,7);
 INSERT INTO totalequipment VALUES ('Burette',10,10,15);
 INSERT INTO totalequipment VALUES ('Pipette',10,10,15);
-INSERT INTO totalequipment VALUES ('Watchglass',15,15,5)
+INSERT INTO totalequipment VALUES ('Watchglass',15,15,5);
 INSERT INTO totalequipment VALUES ('Flask',20,20,10);
